7a50dca045bf1247335a4798f669ba13453f36dc
Bob Clarke <bobmclarke@gmail.com>
2018-05-09 17:59:29 +0100

    Working out how terraform uses local AWS_ env vars

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,30 +1,51 @@
-resource "aws_iam_role" "route53_mapper" {
-  name = "route53_mapper"
+#resource "aws_iam_group" "kops" {
+#  name = "kops"
+#}
 
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Effect": "Allow",
-     "Action": [
-       "route53:ChangeResourceRecordSets"
-     ],
-     "Resource": [
-       "arn:aws:route53:::hostedzone/*"
-     ]
-   },
-   {
-     "Effect": "Allow",
-     "Action": [
-       "route53:ListHostedZones",
-       "route53:ListResourceRecordSets"
-     ],
-     "Resource": [
-       "*"
-     ]
-   }
- ]
+resource "aws_iam_policy_attachment" "kops" {
+  name       = "kops"
+  groups     = ["kops"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
-EOF
+
+resource "aws_iam_policy_attachment" "kops-route53" {
+  name       = "kops"
+  groups     = ["kops"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
+resource "aws_iam_policy_attachment" "kops-s3" {
+  name       = "kops"
+  groups     = ["kops"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_policy_attachment" "kops-iam" {
+  name       = "kops"
+  groups     = ["kops"]
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_policy_attachment" "kops-vpc" {
+  name       = "kops"
+  groups     = ["kops"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+#resource "aws_iam_user" "kops" {
+#  name = "kops"
+#}
+
+resource "aws_iam_group_membership" "kops" {
+  name = "kops"
+  users = [
+    "kops",
+  ]
+  group = "kops"
+}
+
+resource "aws_s3_bucket" "kops" {
+  #bucket = "gdp-devops-k8s.nonprod.aws.dentsufusion.com"
+  bucket = "gdp-devops-test"
+  region = "ca-central-1"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,8 +18,7 @@ variable "aws_amis" {
   default = {
     eu-west-1 = "ami-674cbc1e"
     us-east-1 = "ami-2757f631"
-    us-west-1 = "ami-969ab1f6"
     us-west-2 = "ami-8803e0f0"
+    ca-central-1 = "ami-a954d1cd"
   }
 }
-
